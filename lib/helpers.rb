72e69da0d7444600c0363ef3e6fb8d3025fbf93a
Olivier Patry <olivier.patry@gmail.com>
2024-05-19 19:38:43 +0200

    Custom rating star rendering

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -26,14 +26,18 @@ def book?(item)
   item.identifier =~ '/book/*'
 end
 
-def star_svg(filled:)
-  bg_color = filled ? '#FFEA00' : '#D2D2D2'
-  fg_color = filled ? '#C1AB60' : '#686868'
-  %Q[<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 512 512" xml:space="preserve" style="padding: 0px 1px;"><polygon fill="#{bg_color}" stroke="#{fg_color}" stroke-width="37.6152" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="259.216,29.942 330.27,173.919 489.16,197.007 374.185,309.08 401.33,467.31 259.216,392.612 117.104,467.31 144.25,309.08 29.274,197.007 188.165,173.919 "></polygon></svg>]
+def star_svg(filled:, params: {})
+  size = params.fetch(:size, 24)
+  width = params.fetch(:width, size)
+  height = params.fetch(:height, size)
+  %Q[<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="#{width}" height="#{height}" viewBox="0 0 576 512" class="star-icon #{filled ? 'icon-active' : 'icon-inactive'}"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>]
 end
 
-def heart_svg
-  %Q[<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"class="heart-icon" ><path d="M12 20a1 1 0 0 1-.437-.1C11.214 19.73 3 15.671 3 9a5 5 0 0 1 8.535-3.536l.465.465.465-.465A5 5 0 0 1 21 9c0 6.646-8.212 10.728-8.562 10.9A1 1 0 0 1 12 20z"></path></svg>]
+def heart_svg(params: {})
+size = params.fetch(:size, 24)
+  width = params.fetch(:width, size)
+  height = params.fetch(:height, size)
+  %Q[<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="#{width}" height="#{height}" viewBox="0 0 24 24" class="heart-icon"><path d="M12 20a1 1 0 0 1-.437-.1C11.214 19.73 3 15.671 3 9a5 5 0 0 1 8.535-3.536l.465.465.465-.465A5 5 0 0 1 21 9c0 6.646-8.212 10.728-8.562 10.9A1 1 0 0 1 12 20z"/></svg>]
 end
 
 def page_title(item)
