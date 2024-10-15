f87cfb84e04d7ee904857a1c06452ce5535002a6
Olivier Patry <olivier.patry@gmail.com>
2024-10-15 08:31:05 +0200

    Try mkdir before :thumbnailize

--- lib/filters/thumbnailize.rb
+++ lib/filters/thumbnailize.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
+require 'fileutils'
+
 class Thumbnailize < Nanoc::Filter
   identifier :thumbnailize
   type       :binary
 
   def run(filename, params = {})
+    output_dir = File.dirname(output_filename)
+    FileUtils.mkdir_p(output_dir) unless Dir.exist?(output_dir)
     system(
       'magick',
       filename,
