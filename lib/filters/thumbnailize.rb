8b5d06187aed6a74d0142cc6d75da13b8aa69500
Olivier Patry <olivier.patry@gmail.com>
2024-10-15 08:52:08 +0200

    Clean CI logs

--- lib/filters/thumbnailize.rb
+++ lib/filters/thumbnailize.rb
@@ -7,11 +7,6 @@ class Thumbnailize < Nanoc::Filter
   type       :binary
 
   def run(filename, params = {})
-    output_dir = File.dirname(output_filename)
-    puts "output_dir1=#{output_dir} -> #{Dir.exist?(output_dir)}"
-    FileUtils.mkdir_p(output_dir) unless Dir.exist?(output_dir)
-    puts "filename=#{filename} -> #{output_filename}"
-    puts "output_dir2=#{output_dir} -> #{Dir.exist?(output_dir)}"
     system(
       'magick',
       filename,
