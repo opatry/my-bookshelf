34181f646ea02c0cddc15961c058de90f2cb1bd3
Olivier Patry <olivier.patry@gmail.com>
2025-03-08 15:01:54 +0100

    Fix commit_book.sh script following isbn removal from front matter

--- commit_book.sh
+++ commit_book.sh
@@ -7,8 +7,7 @@ origin=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) || exit
 new_books=$(cd "${origin}/content/book" && git ls-files -o --exclude-standard --full-name)
 
 for book in ${new_books}; do
-  name="$(basename "${book}")"
-  name="${name%.*}"
+  name="$(basename "${book}" .md)"
   git add "${book}"
   git add "${origin}/content/cover/${name}."*
 
@@ -16,9 +15,7 @@ for book in ${new_books}; do
   title=${title#"title: "}
   author=$(grep "^author:\ *" < "${book}")
   author=${author#"author: "}
-  isbn=$(grep "^isbn:\ *" < "${book}")
-  isbn=${isbn#"isbn: "}
-  isbn=$(cut -d'"' -f2 <<< "${isbn}")
+  isbn="${name}"
   commit_msg="[book][${isbn}] ${title} by ${author}"
 
   # use book read date as Git author date (not committer date on purpose)
