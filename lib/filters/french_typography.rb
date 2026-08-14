546948acfafd0f403829b632956616f88fb270f1
Olivier Patry <olivier.patry@gmail.com>
2026-08-14 13:22:56 +0200

    Add unbreakable spaces before french double punctuation

---
+++ lib/filters/french_typography.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'nokogiri'
+
+class FrenchTypographyFilter < Nanoc::Filter
+  identifier :french_typography
+
+  NO_BREAK_SPACE = "\u00a0"
+  ELLIPSIS = "\u2026"
+
+  def run(content, params = {})
+    content
+      .gsub('...', ELLIPSIS)
+      .gsub(/[ \t]+([;:!?])/, "#{NO_BREAK_SPACE}\\1")
+  end
+end
